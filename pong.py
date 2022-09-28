09d313b219c5635337e07f9860fa3691f774644a
Meredith Seaberg <meredithseabergaccount@Merediths-MacBook-Pro.local>
2022-09-28 15:03:59 -0400

    auth

--- pong.py
+++ pong.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, session, redirect, url_for
+from flask import Flask, render_template, session, redirect, url_for, jsonify
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -8,10 +8,35 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 
 import random
+from flask_httpauth import HTTPDigestAuth
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret key here'
+auth = HTTPDigestAuth()
+
+users = {
+    "vcu": "rams"
+}
+
+@auth.get_password
+def get_pw(username):
+    if username in users:
+        return users.get(username)
+    return None
+
 
 @app.route('/', methods=['GET'])
+@auth.login_required
 def index():
     r = random.randint(1, 100)
     return str(r)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'message':'Page Not Here'}), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({'message':'Something is Broke'}), 500
